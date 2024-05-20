944d9e3c9d51b8e6a7209ac4eede768d57d8eea6
GuilhermeNeris <guilherme.neris@sptech.school>
2024-05-19 21:22:59 -0300

    Scripts organizados

---
+++ Site/js/SobreMim.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location.href = 'index.html';
+}
+
+function login(){
+    window.location.href = 'telaCadastro.html'
+}
+
+function cubePlay(){
+    window.location.href = 'index.html#meuFrame';
+}
+
+function inicio(){
+    window.location.href = 'index.html';
+}
